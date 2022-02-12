ff9405a0ce2c8f1c3fdee54b111e6297fb28daaa
Spirina_Anastasia <holoduned@mail.ru>
2022-02-12 19:31:50 +0300

    .

---
+++ Домашние, 2 семестр/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Домашние__2_семестр
+{
+    internal class Class1
+    {
+    }
+}
