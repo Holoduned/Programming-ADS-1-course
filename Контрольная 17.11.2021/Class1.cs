7be4f53fd8f4edd8147faf11b2bf7e2c9f04ac46
Spirina_Anastasia <holoduned@mail.ru>
2022-02-12 19:29:21 +0300

    .

--- Контрольная 17.11.2021/Class1.cs
+++ Контрольная 17.11.2021/Class1.cs
@@ -6,25 +6,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Study.NewFolder2
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+//namespace Study.NewFolder2
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
 
-            Console.Write("Введите количество людей на занятии: ");
-            int count = Convert.ToInt32(Console.ReadLine());
-            Student[] students = Student.InputInfo(count);
+//            Console.Write("Введите количество людей на занятии: ");
+//            int count = Convert.ToInt32(Console.ReadLine());
+//            Student[] students = Student.InputInfo(count);
 
-            Console.Write("Введите количество вопросов, которые будут заданы на занятии: ");
-            int count_question = Convert.ToInt32(Console.ReadLine());
+//            Console.Write("Введите количество вопросов, которые будут заданы на занятии: ");
+//            int count_question = Convert.ToInt32(Console.ReadLine());
 
-            Desk desk = new Desk(count_question);
-            desk.WorkAtDesk(students);
+//            Desk desk = new Desk(count_question);
+//            desk.WorkAtDesk(students);
 
-            Console.ReadKey();
+//            Console.ReadKey();
 
-        }
-    }
-}
+//        }
+//    }
+//}
