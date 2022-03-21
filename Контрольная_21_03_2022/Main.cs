d07d528ddbdbde9f52df27af1227cd4b4f21923f
Holoduned <94117859+Holoduned@users.noreply.github.com>
2022-03-21 18:30:16 +0300

    перепутала main'ы :')

--- Контрольная_21_03_2022/Main.cs
+++ Контрольная_21_03_2022/Main.cs
@@ -1,43 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//namespace Programming.Кынтрыльная_заготовки
-//{
-//    class Programm
-//    {
-//        static void Main(string[] args)
-//        {
-//            //DoublyLinkedList<int> list = new DoublyLinkedList<int>();
-//            //list.Add(1);
-//            //list.Add(2);
-//            //list.Add(3);
-//            //list.AddFirst(5);
-//            //list.RemoveLast();
-//            //list.RemoveFirst();
-//            //Console.WriteLine(String.Join(" ", list.Select(x => x.Data)));
+namespace Контрольная_21_03_2022
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            CustomQueue<int> queue = new CustomQueue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            Console.WriteLine(String.Join(" ", queue.BackEnumerator()));
+            queue.Remove(3);
+            queue.Dequeue();
+            queue.PrintQueue();
 
-//            //CircularOneLinkedList<int> list2 = new CircularOneLinkedList<int>();
-//            //list2.Add(4);
-//            //list2.Add(5);
-//            //list2.Add(6);
-//            //list2.Add(7);
-//            //list2.AddFirst(6);
-//            //Console.WriteLine(list2.First().Data);
-//            ////Console.WriteLine(list2.Last().Data);
-
-//            //list2.RemoveFirst();
-//            //Console.WriteLine(list2.First().Data);
-
-//            CircularTwoLinkedList<int> list3 = new CircularTwoLinkedList<int>();
-//            list3.Add(7);
-//            list3.Add(8);
-//            list3.Add(9);
-//            list3.AddFirst(50);
-//            list3.RemoveLast();
-//            list3.Last().Data.Print();
-//        }
-//    }
-//}
+            Console.WriteLine(queue.Size());
+            Console.WriteLine(queue.IsEmpty());
+        }
+    }
+}
