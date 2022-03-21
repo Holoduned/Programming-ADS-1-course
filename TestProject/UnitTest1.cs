ce11f47c136a36c6a30cbcf255c18156893bdbad
Spirina_Anastasia <holoduned@mail.ru>
2022-03-21 14:51:21 +0300

    .

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,57 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Programming;
+using System.Collections;
+using System;
+using System.Linq;
+using Контрольная_21_03_2022;
+
+namespace Test
+{
+    [TestClass]
+    public class CollectionTest
+    {
+        [TestMethod]
+        public void TestAdding()
+        {
+            CustomQueue<int> queue = new CustomQueue<int>();
+            queue.Enqueue(1);
+
+            Assert.AreEqual(queue.ToString(), "1");
+        }
+
+        [TestMethod]
+        public void TestDeleting()
+        {
+            CustomQueue<int> queue = new CustomQueue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Dequeue();
+
+            Assert.AreEqual(queue.ToString(), "1");
+        }
+
+        [TestMethod]
+        public void TestIsEmpty()
+        {
+            CustomQueue<int> queue = new CustomQueue<int>();
+            Assert.IsTrue(queue.IsEmpty());
+
+            queue.Enqueue(1);
+            Assert.IsFalse(queue.IsEmpty());
+        }
+
+        [TestMethod]
+        public void TestSize()
+        {
+            CustomQueue<int> queue = new CustomQueue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+
+            var array = new Node<int>[] { new Node<int>(1), new Node<int>(2), new Node<int>(3) };
+            Assert.AreEqual(queue, array);
+            queue.Enqueue(5);
+            Assert.AreNotEqual(queue, array);
+        }
+    }
+}
