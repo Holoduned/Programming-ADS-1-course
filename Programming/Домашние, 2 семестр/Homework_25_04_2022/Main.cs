e188e5ee718d2b3513fe821e841924f102b5e6c0
Spirina_Anastasia <holoduned@mail.ru>
2022-04-25 21:31:59 +0300

    .

---
+++ Programming/Домашние, 2 семестр/Homework_25_04_2022/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+namespace Programming.Programming.Домашние__2_семестр.Homework_25_04_2022;
+
+class Programm
+{
+    static void Main(string[] args)
+    {
+        var PowerStation = new PowerStationClass(50);
+        var FireStation = new Firefighters();
+        var MES = new MinistryEmergencySituations();
+        
+        PowerStation.Accident += FireStation.GoOnAccident;
+        PowerStation.Accident += MES.GoOnAccident;
+        
+        PowerStation.Start();
+    }
+}
---
+++ Programming/Домашние, 2 семестр/Homework_25_04_2022/Power plant Class.cs
@@ -0,0 +1,58 @@
+﻿namespace Programming.Programming.Домашние__2_семестр.Homework_25_04_2022;
+
+public class PowerStationClass
+{
+    private double current_temperature;
+    private double critical_temperature = 360;
+    private double heatingSpeed = 60;
+    
+    public delegate void MethodContainer();
+    public event MethodContainer  Accident;
+    
+    private System.Timers.Timer TickTimer;
+
+    public PowerStationClass(double temperature)
+    {
+        current_temperature = temperature;
+    }
+    
+    public void Start()
+    {
+        Console.WriteLine($"Текущая температура: {current_temperature} / {critical_temperature}");
+        TickTimer = new System.Timers.Timer(3000);
+        
+        TickTimer.Elapsed += TimeHeating;
+        
+        TickTimer.Start();
+        Console.ReadLine();
+        TickTimer.Stop();
+    }
+    
+    private void TimeHeating(object source, EventArgs e)
+    {
+        current_temperature += heatingSpeed;
+        if (current_temperature >= critical_temperature)
+        {
+            Accident();
+            current_temperature = 0;
+        }
+        Console.WriteLine($"Текущая температура: {current_temperature} / {critical_temperature}");
+    }
+}
+
+public class Firefighters
+{
+    public void GoOnAccident()
+    {
+        Console.WriteLine("Пожарная служба выехала на место происшествия!"); 
+    }                                                        
+}
+
+public class MinistryEmergencySituations
+{
+    public void GoOnAccident()
+    {
+        Console.WriteLine("МЧС выехала на место происшествия!"); 
+    }   
+}
+
