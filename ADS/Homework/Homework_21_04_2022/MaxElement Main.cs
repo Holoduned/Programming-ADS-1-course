5d4aecac442333971d166beec3ebc82b432c76a3
Spirina_Anastasia <holoduned@mail.ru>
2022-04-25 13:51:59 +0300

    .

--- ADS/Homework/Homework_21_04_2022/MaxElement Main.cs
+++ ADS/Homework/Homework_21_04_2022/MaxElement Main.cs
@@ -4,19 +4,18 @@ public class Program
 {
     static void Main(string[] args)
     {
-        var array = new int[]{1, 9, 6, 20, 8, 2};
-        Console.WriteLine("f");
+        var array = new int[]{1001, 9, 6, 20, 8, 12, 45, 2, 4, 900, 4, 6, 1000};
         Console.WriteLine(FindMax(array, 0, array.Length - 1));
     }
     static int FindMax(int[] data, int start, int end)
     {
-        if (start - end <= 1) 
+        if (end - start <= 1) 
         {
             return Math.Max(data[start], data[end]);
         }
         int mid = (start + end) / 2; 
-        int leftMax =  FindMax(data[..mid], start, mid);
-        int rightMax = FindMax(data[mid..],mid +1, end);
+        int leftMax =  FindMax(data[..mid], start, data[..mid].Length - 1);
+        int rightMax = FindMax(data[mid..], 0 , data[mid..].Length - 1);
         return Math.Max(leftMax, rightMax);
     }
 }
--- Programming/Домашние, 2 семестр/Homework_11_04-18_04.2022/Main avl Tree.cs
+++ Programming/Домашние, 2 семестр/Homework_11_04-18_04.2022/Main avl Tree.cs
@@ -1,4 +1,4 @@
-﻿namespace Programming.Programming.Домашние__2_семестр.Homework_11_04_18_04._2022;
+﻿/*namespace Programming.Programming.Домашние__2_семестр.Homework_11_04_18_04._2022;
 
 public class Programm
 {
@@ -13,21 +13,6 @@ public class Programm
         // }
         //
         // Tree.PrintDepth();
-        
-        var array = new int[]{1001, 9, 6, 20, 8, 12, 45, 2, 4, 900, 4, 6, 1000};
-        Console.WriteLine("f");
-        Console.WriteLine(FindMax(array, 0, array.Length - 1));
     }
     
-    static int FindMax(int[] data, int start, int end)
-    {
-        if (end - start <= 1) 
-        {
-            return Math.Max(data[start], data[end]);
-        }
-        int mid = (start + end) / 2; 
-        int leftMax =  FindMax(data[..mid], start, data[..mid].Length - 1);
-        int rightMax = FindMax(data[mid..], 0 , data[mid..].Length - 1);
-        return Math.Max(leftMax, rightMax);
-    }
-}
+}*/
